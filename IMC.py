83c242e613b7913315bdb9ef86b25b79c5adce48
MARCELO GOMES CASTANHO <71048436+marcelo-prog@users.noreply.github.com>
2021-01-30 16:35:46 -0300

    Create IMC.py

---
+++ IMC.py
@@ -0,0 +1,4 @@
+peso = float(input('Digite seu peso.'))
+altura = float(input('Digite sua altura.'))
+imc = peso / (altura*altura)
+print('O seu indice de massa corporal ou simplesmente "IMC" é',imc)
